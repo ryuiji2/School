d905e1757e88b537de887cc55b66dd51869a168c
Rief Haalboom <superrieff@hotmail.com>
2017-10-17 13:48:00 +0200

    begin van snake

---
+++ Unity/Snoke/Assets/Scripts/movement.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class movement : MonoBehaviour {
+
+    Transform playerTrans;
+
+	void Start () {
+        StartCoroutine (moving ());
+        }
+	
+	void Update () {
+
+        Rotation();
+	}
+    void Rotation () {
+
+        if (Input.GetButtonDown ("w")) {
+            transform.eulerAngles = new Vector3 (0, 0, 0);
+            }
+        if (Input.GetButtonDown ("a")) {
+            transform.eulerAngles = new Vector3 (0, -90, 0);
+            }
+        if (Input.GetButtonDown ("d")) {
+            transform.eulerAngles = new Vector3 (0, 90, 0);
+            }
+        if (Input.GetButtonDown ("s")) {
+            transform.eulerAngles = new Vector3 (0, -180, 0);
+            }
+        }
+
+    IEnumerator moving() {
+        transform.position += transform.forward;
+        yield return new WaitForSeconds (0.3f);
+        StartCoroutine (moving ());
+        }
+}
