ed5951d4001c8b9da7ae0e65207142fade0eb2ee
Rief Haalboom <superrief@hotmail.com>
2017-11-14 04:20:56 +0100

    Nature Scene

---
+++ Unity/Nature scene/Assets/Movement Code/CameraMove.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour {
+
+    public float mouseH;
+    public float mouseV;
+    private float hor;
+    private float ver;
+    public float moveSpeed;
+
+
+	
+	void Update () {
+
+        MouseRot();
+        Movement();
+	}
+
+    void MouseRot() {
+
+        hor += mouseH * Input.GetAxis("Mouse X");
+        ver += -mouseV * Input.GetAxis("Mouse Y");
+
+        transform.eulerAngles = new Vector3(ver, hor, 0.0f);
+    }
+    void Movement() {
+        float forwardBack = Input.GetAxis("Vertical") * moveSpeed * Time.deltaTime;
+        float leftRight = Input.GetAxis("Horizontal") * moveSpeed * Time.deltaTime;
+
+        transform.Translate(leftRight, 0, forwardBack);
+    }
+}
