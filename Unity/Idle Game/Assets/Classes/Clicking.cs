1ec33894bdf4796988f627b4a5e4041d0967f2b3
Rief Haalboom <superrieff@hotmail.com>
2017-11-28 11:40:47 +0100

    Idle Game

---
+++ Unity/Idle Game/Assets/Classes/Clicking.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Clicking : MonoBehaviour {
+
+    public int purchased;
+    public int startPrice;
+    public int costToBuy;
+    public int moneyToGet;
+    public Text moneyText;
+
+	void Start () {
+		
+
+
+	}
+    void ButtonClick () {
+        purchased++;
+        costToBuy = startPrice * purchased;
+
+    }
+}
