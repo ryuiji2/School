5ceecb105845a9541a0fcfeb35b7f62bf2d7dc85
Rief Haalboom <superrief@hotmail.com>
2018-01-30 18:21:20 +0100

    prototype pickup pet

---
+++ Unity/Pickup Pet/Assets/Scripts/SpawnDrops.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawnDrops : MonoBehaviour {
+
+    public GameObject pet;
+    public GameObject redCube;
+
+	void Start () {
+		
+	}
+	
+	void Update () {
+        
+        if (Input.GetButtonDown("use")){
+            GameObject red =  Instantiate(redCube, new Vector3(1, 1, 1), Quaternion.identity);
+           
+        }
+	}
+}
